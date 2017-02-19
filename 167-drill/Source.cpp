fb8262a935e7085b9e6ab9e20a90aaad47f7f0b7
Mark.Alexieff <Mark.Alexieff@INA-8PMPKX1>
2017-02-18 21:04:27 -0800

    Initial Scafford

---
+++ 167-drill/Source.cpp
@@ -0,0 +1,45 @@
+/*
+Scaffolding for doing the drill on page 167
+*/
+
+// Start standsard opening
+
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+
+using namespace std;
+
+inline void keep_window_open() { char ch; cin >> ch; }
+
+inline void simple_error(string s)	// write ``error: s and exit program
+{
+	cerr << "error: " << s << '\n';
+	keep_window_open();		// for some Windows environments
+	exit(1);
+}
+
+// End standard opening
+
+// Begin Scaffolding
+int main() {
+	try {
+		// my code here
+
+		keep_window_open();
+		return 0;
+	}
+	catch (exception& e) {
+		cerr << "error: " << e.what() << '\n';
+		keep_window_open();
+		return 1;
+	}
+	catch (...) {
+		cerr << "Oops: unknown exception!\n";
+		keep_window_open();
+		return 2;
+	}
+}
